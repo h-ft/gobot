078942bd79f2f3259eae88d92fd0c7a589220e84
h-ft <henryft51@gmail.com>
2022-01-24 21:00:25 +0700

    Added function to track crypto prices from an open API

--- cmd/connection.go
+++ cmd/connection.go
@@ -40,7 +40,4 @@ func connect(token string) {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-
-	// Cleanly close down the Discord session.
-	dg.Close()
 }
--- cmd/handler.go
+++ cmd/handler.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/sirupsen/logrus"
 	"main.go/files/helper"
+	"main.go/files/request"
 )
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
-func messageCreate(s *discordgo.Session, m helper.IncomingMessage) {
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	err := m.CheckPrefix()
+	logrus.Info("Processing msg : ", m.Content, " from ", m.Author.Username)
+
+	err := helper.CheckPrefix(m.Content)
 	if err != nil {
-		fmt.Println("Error: ", err)
+		logrus.Info("Message is not a command")
+		return // Do nothing if the message is not a command
 	}
 
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	msg := strings.TrimSpace(m.Content)
+	logrus.Info(msg)
+	command := strings.Split(msg[1:], " ")
+	logrus.Info(command)
+	if len(command) == 1 {
+		switch command[0] {
+		case "btc", "bitcoin":
+			s.ChannelMessageSend(m.ChannelID, request.GetTokenByID(90))
+			return
+		}
 	}
 }
